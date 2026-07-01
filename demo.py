210f5a1e743a7cb74092726bd51eabd1109a2d76
Pratima Kadam <pratimakadam308@gmail.com>
2026-07-01 15:42:36 +0530

    New Information Added

--- demo.py
+++ demo.py
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 print("Hello")
-
-
-=======
 print("sonal here")
->>>>>>> 27186c9808cd6bbd785289e85017f4b677118a05
-print("Hello  Everyone !I am Dimpal Rajput")
+print("Hello Everyone !I am Dimpal Rajput")
+print("Hello,I am Pratima")
