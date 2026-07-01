0119556c55ea1211f1f42aaecb8ce885b9232398
dhanshri salunkhe <dhanshrisalunkhe27@gmail.com>
2026-07-01 10:43:54 +0530

    update demo.py

--- demo.py
+++ demo.py
@@ -1,4 +1,9 @@
 print("Hello")
+<<<<<<< HEAD
 print("sonal here")
 print("Hello Everyone !I am Dimpal Rajput")
 print("Hello,I am Pratima")
+=======
+print("Dhanshri salunkhe")
+
+>>>>>>> 8a1634e (update demo.py)
