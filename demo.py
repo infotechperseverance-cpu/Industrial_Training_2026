5a99a0a242cb03bbd6c210a6a5025ab2825c387e
chavhansakshi560-beep <chavhansakshi560@gmail.com>
2026-07-02 07:56:40 +0530

    Updated demo.py

--- demo.py
+++ demo.py
@@ -1,8 +1,2 @@
 print("Hello")
-
-print("sonal here")
-print("Hello Everyone !I am Dimpal Rajput")
-print("Hello,I am Pratima")
-print("Dhanshri salunkhe")
-
-
+print("Hello Everyone,I am Sakshi Chavhan")
