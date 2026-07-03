d57e37558d41e537c5e546323c8212746aba3962
bhavsarpriya254-ux <bhavsarpriya254@gmail.com>
2026-07-03 13:45:46 +0530

    Enhance notification system with speech output
    Added text-to-speech functionality to task reminders.

--- AVENGERS/AUTOMATIC_TASK_SCHEDULER/SRC/Notification.py
+++ AVENGERS/AUTOMATIC_TASK_SCHEDULER/SRC/Notification.py
@@ -1,11 +1,19 @@
 import os
 import csv
 import time
+import pyttsx3
+import threading
 from datetime import datetime
 import schedule
 from plyer import notification
+
+engine = pyttsx3.init()
+engine = setProperty('rate', 160)
+engine = setProperty('volume', 1.0)
+
 def send_popup(task_title):
 	notification.notify(title="Task Reminder",message=task_title,timeout=10 )
+	threading.Thread(target=speak_task, args=(task_title,),daemon=True).start)
 
 def setup_scheduler():
 	if os.path.exists("Tasks.csv"):
@@ -15,6 +23,10 @@ def setup_scheduler():
 			task_title = row[0]
 			target_time = row[2]
 			schedule.every().day.at(target_time).do(send_popup, task_title=task_title)
+def speak_task(task_title):
+	engine.say(f"Reminder.{task_tittle}")
+	engine.runAndWait()
+
 setup_scheduler()
 print("Notification scheduler is running...")
 while True:
